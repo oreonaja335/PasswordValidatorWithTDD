a74ce7078ac542aa23c281cd92679c990725d4d2
oreonaja335 <eieioreo75@gmail.com>
2025-07-07 16:47:20 +0700

    Add files via upload

--- lib/PasswordValidator.java
+++ lib/PasswordValidator.java
@@ -3,17 +3,39 @@ package lib;
 public class PasswordValidator {
 
     /**
-     * คุณจะต้องเขียน Javadoc ที่สมบูรณ์ที่นี่ในอนาคต
-     * เพื่ออธิบายกฎการทำงานของเมธอด
+     * @param รับข้อมูล password ที่เป็น String
+     * @return ส่งค่าข้อมูลกลับที่เป็น invalid เเละค่าอื่น ๆ
      */
-    // TODO: แก้ไข return type ของเมธอดนี้ให้ถูกต้อง
-    public static void validate(String password) { // Function Type ให้เป็น PasswordStrength 
+   
+    public static PasswordStrength validate(String password) { 
         
-        // ส่วนของ Implementation ที่คุณต้องเขียน
-        // ...
+        if( password == null || password.length() < 8 ){
+            return PasswordStrength.INVALID;
+        } 
+            
+        boolean Lower = false ;
+        boolean Upper = false ;
+        boolean Special = false ;
+        boolean Digit =  false ;
+
+        for(char c : password.toCharArray()){
+            if(Character.isLowerCase(c)){
+                Lower = true ;
+            }else if(Character.isUpperCase(c)){
+                Upper = true ;
+            }else if(Character.isDigit(c)){
+                Digit = true ;
+            }else if(!Character.isLowerCase(c) && !Character.isUpperCase(c) && !Character.isDigit(c)){
+                Special = true ;
+            }
+        }
+
+        if(Lower && !Upper && !Special && !Digit) return PasswordStrength.WEAK;
+        if(Lower && Upper && !Special && !Digit) return PasswordStrength.MEDIUM;
+        if(Lower && Upper && Special && Digit) return PasswordStrength.STRONG;
         
-        int minLength = 8 // TODO: มีอะไรขาดหายไปที่บรรทัดนี้?
         
-        return null ; // TODO: การคืนค่านี้ถูกต้องหรือไม่?
+        return PasswordStrength.INVALID; 
+        
     }
 }
--- lib/PasswordValidatorTest.java
+++ lib/PasswordValidatorTest.java
@@ -6,14 +6,41 @@ public class PasswordValidatorTest{
         System.out.println("--- Running Password Validator Tests ---");
         
         // Test Case 1: รหัสผ่านสั้นควรจะ INVALID
-        PasswordStrength result1 = PasswordValidator.validate("123");
+        String pw = "oreo" ;
+
+        PasswordStrength result1 = PasswordValidator.validate(pw);
         if (result1 == PasswordStrength.INVALID) {
-            System.out.println("Test Case 1 Passed: Short password is INVALID.");
+            System.out.println("Test Case 1 Passed : "+pw+" Short password is INVALID.");
         } else {
-            System.out.println("Test Case 1 FAILED: Expected INVALID but got " + result1);
+            System.out.println("Test Case 1 FAILED : "+pw+" Expected INVALID but got " + result1);
+        }
+        
+        // Test Case 2: รหัสผ่านที่มีตัวอักษรพิมเล็กทั้งหมดจะ WEAK
+        pw = "sasikann" ;
+        PasswordStrength result2 = PasswordValidator.validate(pw);
+        if (result2 == PasswordStrength.WEAK) {
+            System.out.println("Test Case 2 Passed : "+pw+" Lower password is WEAK.");
+        } else {
+            System.out.println("Test Case 2 FAILED : "+pw+" Expected WEAK but got " + result2);
         }
 
-        // --- เขียน Test Case อื่นๆ ต่อ ---
+         // Test Case 3: รหัสผ่านที่มีตัวอักษรพิมเล็กเเละพิมพ์ใหญ่จะ MEDIUM
+        pw = "Sasikann" ;
+        PasswordStrength result3 = PasswordValidator.validate(pw);
+        if (result3 == PasswordStrength.MEDIUM) {
+            System.out.println("Test Case 3 Passed : "+pw+" Lower and Upper password is MEDIUM.");
+        } else {
+            System.out.println("Test Case 3 FAILED : "+pw+" Expected MEDIUM but got " + result3);
+        }
+         // Test Case 4: รหัสผ่านที่มีตัวอักษรพิมเล็กเเละพิมพ์ใหญ่เเละตัวเลขเเละอักษรพิเศษจะ STRONG
+        pw = "Sasikan_09" ;
+        PasswordStrength result4 = PasswordValidator.validate(pw);
+        if (result4 == PasswordStrength.STRONG) {
+            System.out.println("Test Case 4 Passed : "+pw+" Lower and Upper and digit and special password is STORNG.");
+        } else {
+            System.out.println("Test Case 4 FAILED : "+pw+" Expected STRONG but got " + result4);
+        }
+
 
         System.out.println("--------------------------------");
     }
